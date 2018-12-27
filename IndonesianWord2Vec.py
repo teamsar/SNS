a9b826e02f54c64ae32a764598d79abd6180aa80
teamsar <teams.muliadi@gmail.com>
2018-12-27 06:03:26 +0000

    Implementing word2vec on political corpus data

--- IndonesianWord2Vec.py
+++ IndonesianWord2Vec.py
@@ -1,15 +1,28 @@
+import os
+import nltk
 from gensim.models import Word2Vec
-from nltk.corpus import brown, movie_reviews, treebank
+from nltk.corpus import movie_reviews
 
 # b = Word2Vec(brown.sents())
 # mr = Word2Vec(movie_reviews.sents())
 # t = Word2Vec(treebank.sents())
 
 if __name__ == '__main__':
-    data = [['teamsar', 'muliadi', 'hobi', 'makan', 'nasi', 'remes'],
-            ['angga', 'sanjaya', 'lingga', 'makan', 'tahu', 'isi'],
-            ['disana', 'ada', 'jual', 'makanan', 'ringan', 'enak'], ['tidak', 'enak', 'kalau', 'masak', 'sendiri'],
-            ['kutunggu', 'makanan', 'bergizi']]
+    # data = [['teamsar', 'muliadi', 'hobi', 'makan', 'nasi', 'remes'],
+    #         ['angga', 'sanjaya', 'lingga', 'makan', 'tahu', 'isi'],
+    #         ['disana', 'ada', 'jual', 'makanan', 'ringan', 'enak'], ['tidak', 'enak', 'kalau', 'masak', 'sendiri'],
+    #         ['kutunggu', 'makanan', 'bergizi']]
+    data = []
+    for root, dirs, files in os.walk("..\SNS\political_corpus\\"):
+        for file in files:
+            if not file.endswith(".py") and not file.endswith(".txt"):
+                f = open(os.path.join(root, file), 'r', encoding='utf8')
+                lines = f.readlines()
+                for line in lines:
+                    tokens = nltk.word_tokenize(line)
+                    data.append(tokens)
+                # print(os.path.join(root, file))
 
-    wv = Word2Vec(sentences=data, min_count=3)
-    print(wv.most_similar('makan', topn=5))
+    wv = Word2Vec(sentences=data, min_count=2)
+    print(wv['pencurian'], wv.most_similar('pencurian'))
+    # print(wv['makan'], wv.most_similar('makan'))
